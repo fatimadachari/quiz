98e4d22d208fce15fdca178f298d886504bf382e
fatimadachari <fatima.s.dachari@gmail.com>
2024-02-21 14:11:23 -0300

    criação da página inicial e adição do css

---
+++ src/context/quiz.jsx
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+export const QuizContext = createContext();
+
+export const QuizProvider = ({children}) => {
+    const value = {name: 'quiz'};
+    return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
+}
