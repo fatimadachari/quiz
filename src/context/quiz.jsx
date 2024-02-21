4d0e9e6532c0f27234348094fe709c2047782e40
fatimadachari <fatima.s.dachari@gmail.com>
2024-02-21 15:34:30 -0300

    criando a pontuação e verificando as respostas

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -7,19 +7,19 @@ const initialState = {
     gameStage: STAGES[0],
     questions,
     currentQuestion: 0,
-    score: 0
+    score: 0,
+    answerSelected: false,
 }
 
 const quizReducer = (state, action) => {
-    console.log(state, action)
 
-    switch(action.type) {
+    switch (action.type) {
         case "CHANGE_STATE":
             return {
                 ...state,
                 gameStage: STAGES[1],
             };
-        
+
         case "REORDER_QUESTIONS":
             const reorderQuestions = questions.sort(() => {
                 return Math.random() - 0.5;
@@ -29,22 +29,37 @@ const quizReducer = (state, action) => {
                 questions: reorderQuestions,
             };
 
-            case "CHANGE_QUESTION":
-                const nextQuestion = state.currentQuestion + 1;
-                let endGame = false;
+        case "CHANGE_QUESTION":
+            const nextQuestion = state.currentQuestion + 1;
+            let endGame = false;
 
-                if(!questions[nextQuestion]) {
-                    endGame = true;
-                };
+            if (!questions[nextQuestion]) {
+                endGame = true;
+            };
 
-                return {
-                    ...state,
-                    currentQuestion: nextQuestion,
-                    gameStage: endGame ? STAGES[2] : state.gameStage,
-                };
+            return {
+                ...state,
+                currentQuestion: nextQuestion,
+                gameStage: endGame ? STAGES[2] : state.gameStage,
+            };
 
-            case "NEW_GAME":
-                return initialState;
+        case "NEW_GAME":
+            return initialState;
+
+        case "CHECK_ANSWER":
+            if(state.answerSelected) return state; 
+
+            const answer = action.payload.answer;
+            const option = action.payload.option;
+            let correctAnswer = 0;
+
+            if(answer === option) correctAnswer = 1;
+
+            return {
+                ...state,
+                score: state.score + correctAnswer,
+                answerSelected: option,
+            }
 
         default:
             return state;
@@ -53,7 +68,7 @@ const quizReducer = (state, action) => {
 
 export const QuizContext = createContext();
 
-export const QuizProvider = ({children}) => {
+export const QuizProvider = ({ children }) => {
     const value = useReducer(quizReducer, initialState);
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
 }
